d14b855c198d7b1fb0d13d761ce515d4dc96eec0
Pablo Godel <pgodel@gmail.com>
2020-06-27 18:52:36 -0400

    Fix issues with undefined lei or sic

--- src/rest/reference/TickerDetails.php
+++ src/rest/reference/TickerDetails.php
@@ -17,8 +17,8 @@ class TickerDetails extends RestResource {
     }
 
     protected function mapper($response) {
-        $response['legalEntityIdentifier'] = $response['lei'];
-        $response['standardIndustryClassification'] = $response['sic'];
+        $response['legalEntityIdentifier'] = $response['lei'] ?? null;
+        $response['standardIndustryClassification'] = $response['sic'] ?? null;
         return $response;
     }
 }
